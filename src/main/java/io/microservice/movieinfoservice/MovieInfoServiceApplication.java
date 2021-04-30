17a21214e330f01e41c837cff213c9b05d9b7caa
Gias Uddin <bdgias24@gmail.com>
2021-04-30 23:09:23 +0600

    Modified swagger api

--- src/main/java/io/microservice/movieinfoservice/MovieInfoServiceApplication.java
+++ src/main/java/io/microservice/movieinfoservice/MovieInfoServiceApplication.java
@@ -3,7 +3,7 @@ package io.microservice.movieinfoservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,7 +20,8 @@ public class MovieInfoServiceApplication {
     public Docket swaggerConfiguration() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .paths(PathSelectors.ant("/movie/*"))
+//                .paths(PathSelectors.ant("/movie/*"))
+                .apis(RequestHandlerSelectors.basePackage("io.microservice.movieinfoservice.resources"))
                 .build();
     }
 }
--- src/main/java/io/microservice/movieinfoservice/resources/MovieResource.java
+++ src/main/java/io/microservice/movieinfoservice/resources/MovieResource.java
@@ -1,6 +1,7 @@
 package io.microservice.movieinfoservice.resources;
 
 import io.microservice.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/movie")
 public class MovieResource {
 
-    @RequestMapping("/{movieId}")
+    @GetMapping("/{movieId}")
     public Movie getMovie(@PathVariable("movieId") String movieId) {
         return new Movie(movieId, movieId);
     }
